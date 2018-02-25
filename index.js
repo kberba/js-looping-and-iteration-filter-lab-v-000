a8d2178d752ba3958c44528e08e2d4113fd67fd1
Karen Berba <kberba@gmail.com>
2018-02-25 21:11:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file
+
+function findMatching(drivers, string) {
+  let matchDriverList = [];
+  for (const d of drivers) {
+    if (d.toUpperCase() === string.toUpperCase()) {
+      matchDriverList.push(d);
+    }
+  }
+  return matchDriverList;
+}
+
+function fuzzyMatch(drivers, string) {
+  let matchDriverList = [];
+  for (const d of drivers) {
+    if (d.substring(0, 2) === string.substring(0, 2)) {
+      matchDriverList.push(d);
+    }
+  }
+  return matchDriverList;
+}
+
+function matchName(drivers, string) {
+  drivers.filter(function (string) { return })
+}
